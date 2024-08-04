a19eac00fb25e6511cc5ebae649eb49211cc4416
han shen <23018525@myrp.edu.sg>
2024-08-04 12:46:24 +0800

    yes

--- profile.js
+++ profile.js
@@ -3,9 +3,9 @@ window.addEventListener('load', function() {
     const customerId = urlParams.get('id');
 
     const customers = [
-        { id: 1, name: 'John Doe', phone: '1234567890', birthdate: 'January 1, 1990', ic: '12345678', picture: '\Users\nistc\Downloads\john.jpg' },
-        { id: 2, name: 'Jane Smith', phone: '0987654321', birthdate: 'February 2, 1992', ic: '87654321', picture: 'path/to/jane-picture.png' },
-        { id: 3, name: 'Alice Johnson', phone: '5555555555', birthdate: 'March 3, 1988', ic: '11223344', picture: 'path/to/alice-picture.png' },
+        { id: 1, name: 'John Doe', phone: '98367543', birthdate: 'January 1, 1990', ic: '12345678', picture: '\Users\nistc\Downloads\john.jpg' },
+        { id: 2, name: 'Jane Smith', phone: '98765432', birthdate: 'February 2, 1992', ic: '87654321', picture: 'path/to/jane-picture.png' },
+        { id: 3, name: 'Alice Johnson', phone: '96743212', birthdate: 'March 3, 1988', ic: '11223344', picture: 'path/to/alice-picture.png' },
     ];
 
     const customer = customers.find(c => c.id == customerId);
